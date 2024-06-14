8261a607d9f2a50234909d1e579169ccb192f1f9
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-14 22:56:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,99 +6,31 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-# 检查并安装 Node.js 和 npm
-function install_nodejs_and_npm() {
-    if command -v node > /dev/null 2>&1; then
-        echo "Node.js 已安装"
-    else
-        echo "Node.js 未安装，正在安装..."
-        curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-        sudo apt-get install -y nodejs
-    fi
+wget https://github.com/ignite/cli/releases/download/v0.27.1/ignite_0.27.1_linux_amd64.tar.gz
+chmod +x ignite_0.27.1_linux_amd64.tar.gz
+tar -xvf ignite_0.27.1_linux_amd64.tar.gz
+sudo mv ignite /usr/local/bin
+echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
+source ~/.bashrc
+ignite version
+curl -sSL https://raw.githubusercontent.com/allora-network/allora-chain/main/install.sh | bash -s -- v0.0.7
+sudo apt-get update  -y
+sudo apt-get install -y make gcc
+rm -rf /usr/local/go
+curl -OL https://go.dev/dl/go1.22.4.linux-amd64.tar.gz 
+sudo tar -C /usr/local -xvf go1.22.4.linux-amd64.tar.gz
+source ~/.bashrc
+go version
 
-    if command -v npm > /dev/null 2>&1; then
-        echo "npm 已安装"
-    else
-        echo "npm 未安装，正在安装..."
-        sudo apt-get install -y npm
-    fi
-}
-
-# 检查并安装 PM2
-function install_pm2() {
-    if command -v pm2 > /dev/null 2>&1; then
-        echo "PM2 已安装"
-    else
-        echo "PM2 未安装，正在安装..."
-        npm install pm2@latest -g
-    fi
-}
-
-# 检查Go环境
-function check_go_installation() {
-    if command -v go > /dev/null 2>&1; then
-        echo "Go 环境已安装"
-        return 0 
-    else
-        echo "Go 环境未安装，正在安装..."
-        return 1 
-    fi
-}
-# 节点安装功能
-mkdir airchains
-cd airchains
-
-install_nodejs_and_npm
-install_pm2
-
-# 更新和安装必要的软件
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y curl iptables build-essential git wget jq make gcc nano tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev lz4 snapd
-
-# 安装 Go
-if ! check_go_installation; then
-    sudo rm -rf /usr/local/go
-    curl -L https://go.dev/dl/go1.22.0.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
-    echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
-    source $HOME/.bash_profile
-    go version
-fi
-
-# 安装所有二进制文件
 wget https://github.com/airchains-network/junction/releases/download/v0.1.0/junctiond
 chmod +x junctiond
-sudo mv junctiond /usr/local/go/bin
-
-# 配置junctiond
-junctiond config chain-id junction
-junctiond init "Moniker" --chain-id junction
-junctiond config node tcp://localhost:43457
-
-# 获取初始文件和地址簿
-wget -O $HOME/.junction/config/genesis.json https://github.com/airchains-network/junction/releases/download/v0.1.0/genesis.json
-wget https://smeby.fun/airchains-addrbook.json -O $HOME/.junction/config/addrbook.json
-sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025amf\"/;" ~/.junction/config/app.toml
-
-# 配置节点
-SEEDS=""
-PEERS="de2e7251667dee5de5eed98e54a58749fadd23d8@34.22.237.85:26656,1918bd71bc764c71456d10483f754884223959a5@35.240.206.208:26656,48887cbb310bb854d7f9da8d5687cbfca02b9968@35.200.245.190:26656,de2e7251667dee5de5eed98e54a58749fadd23d8@34.22.237.85:26656,8b72b2f2e027f8a736e36b2350f6897a5e9bfeaa@131.153.232.69:26656,e09fa8cc6b06b99d07560b6c33443023e6a3b9c6@65.21.131.187:26656,5c5989b5dee8cff0b379c4f7273eac3091c3137b@57.128.74.22:56256,086d19f4d7542666c8b0cac703f78d4a8d4ec528@135.148.232.105:26656,0305205b9c2c76557381ed71ac23244558a51099@162.55.65.162:26656,3e5f3247d41d2c3ceeef0987f836e9b29068a3e9@168.119.31.198:56256,6a2f6a5cd2050f72704d6a9c8917a5bf0ed63b53@93.115.25.41:26656,eb4d2c546be8d2dc62d41ff5e98ef4ee96d2ff29@46.250.233.5:26656,7d6694fb464a9c9761992f695e6ba1d334403986@164.90.228.66:26656,b2e9bebc16bc35e16573269beba67ffea5932e13@95.111.239.250:26656,23152e91e3bd642bef6508c8d6bd1dbedccf9e56@95.111.237.24:26656,c1e9d12d80ec74b8ddbabdec9e0dad71337ba43f@135.181.82.176:26656,3b429f2c994fa76f9443e517fd8b72dcf60e6590@37.27.11.132:26656,84b6ccf69680c9459b3b78ca4ba80313fa9b315a@159.69.208.30:26656"
+sudo mv junctiond /usr/local/bin
+source ~/.bashrc
+junctiond init moniker
+wget https://github.com/airchains-network/junction/releases/download/v0.1.0/genesis.json
+cp genesis.json ~/.junction/config/genesis.json
+PEERS="e78a440c57576f3743e6aa9db00438462980927e@5.161.199.115:26656,d9a5e20668955bdd5c2fc28cffd6f06e23794638@[2a01:4f8:10a:3a51::2]:43456,61e609631e8be6e04c43ed3d7bfef23c064dc912@[2a01:4f8:221:26e0::2]:43456,f786dcc80601ddd33ba98c609795083ba418d740@158.220.119.11:43456,0b1159b05e940a611b275fe0006070439e5b6e69@[2a03:cfc0:8000:13::b910:277f]:13756,c8f6b1a795a6d9cd2ec39faf277163a9711fc81b@38.242.194.19:43456,ddd9aade8e12d72cc874263c8b854e579903d21c@178.18.240.65:26656,552d2a5c3d9889444f123d740a20237c89711109@109.199.96.143:43456,cc27f4e54a78b950adaf46e5413f92f5d53d2212@209.126.86.186:43456,f5b69a02abeb3340ccd266f049ed6aabc7c0ea88@94.72.114.150:43456"
 sed -i 's|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.junction/config/config.toml
-
-
-# 配置端口
-node_address="tcp://localhost:43457"
-sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:43458\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:43457\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:43460\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:43456\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":43466\"%" $HOME/.junction/config/config.toml
-sed -i -e "s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:43417\"%; s%^address = \":8080\"%address = \":43480\"%; s%^address = \"localhost:9090\"%address = \"0.0.0.0:43490\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:43491\"%; s%:8545%:43445%; s%:8546%:43446%; s%:6065%:43465%" $HOME/.junction/config/app.toml
-echo "export junctiond_RPC_PORT=$node_address" >> $HOME/.bash_profile
-source $HOME/.bash_profile   
-
-pm2 start junctiond -- start && pm2 save && pm2 startup
-
-curl -L http://148.113.6.240/airchains_snapshots.tar.lz4 | tar -I lz4 -xf - -C $HOME/.junction/data
-mv $HOME/.junction/priv_validator_state.json.backup $HOME/.junction/data/priv_validator_state.json
-
-# 使用 PM2 启动节点进程
-pm2 restart junctiond
-echo '====================== 安装完成,已自动退出脚本加载环境变量，未加载未成功，执行 source $HOME/.bash_profile 以加载环境变量==========================='
-kill -INT $$
-source $HOME/.bash_profile
+sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025amf\"/;" ~/.junction/config/app.toml
+screen -S airchains
+junctiond start
